b97063a5a2bc54c7c6daa6c086e5a6717b507d6d
nnra6864 <nnra2210@gmail.com>
2026-08-08 00:28:37 +0200

    feat: Autogenerate config

--- state.lua
+++ state.lua
@@ -1,4 +1,7 @@
+local mp      = require 'mp'
+local utils   = require 'mp.utils'
 local options = require 'mp.options'
+local common  = require 'common'
 
 local M = {
     opts = {
@@ -24,5 +27,36 @@ local M = {
     ffmpeg_ok          = false
 }
 
+local function ensure_config_exists()
+    local opts_dir = mp.command_native({"expand-path", "~~/script-opts"})
+    local user_conf = utils.join_path(opts_dir, "tachytome.conf")
+
+    local f = io.open(user_conf, "r")
+    if f then
+        f:close()
+        return
+    end
+
+    local script_dir = mp.get_script_directory()
+    if not script_dir then return end
+
+    local repo_conf = utils.join_path(script_dir, "tachytome.conf")
+    local rf = io.open(repo_conf, "r")
+    if not rf then return end
+
+    local conf_content = rf:read("*all")
+    rf:close()
+
+    common.ensure_dir(opts_dir)
+    local wf = io.open(user_conf, "w")
+    if wf then
+        wf:write(conf_content)
+        wf:close()
+        mp.msg.info("Generated default config at: " .. user_conf)
+    end
+end
+
+ensure_config_exists()
 options.read_options(M.opts, "tachytome")
+
 return M
